ad6e436c20bd4159b8be1b79381b84871f2f0c21
Oseko Ashpe <oskash6@gmail.com>
2026-08-04 11:04:13 +0300

    Initial commit: Football Analytics RAG assistant

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from data.employees import generate_employee_data
+from data.users import generate_user_data
 import json
 
 if __name__ == "__main__":
 
-    # Here is an example of how to use the get_user_data function
-    users = generate_employee_data(1)[0]
+    # Here is an example of how to use the generate_users function
+    users = generate_user_data(1)[0]
     print("\n\nUser data:")
     print(json.dumps(users, indent=4))
