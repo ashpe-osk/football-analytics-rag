5d8c6ff30050d6267fceb53f3ec42b2edb904f9f
Oseko Ashpe <oskash6@gmail.com>
2026-08-04 12:01:48 +0300

    added new project files

---
+++ template.sh
@@ -0,0 +1,14 @@
+# Creating directory 
+mkdir -p src
+mkdir -p research 
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py 
+touch src/prompt.py
+touch setup.py
+touch research/trials.ipynb 
+
+
+
+echo "Directory and files created successfully!."
