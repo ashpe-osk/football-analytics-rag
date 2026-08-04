23329c03d7db6c347fa743c4a7a94fc43043eb40
Oseko Ashpe <oskash6@gmail.com>
2026-08-04 12:23:03 +0300

    Added requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="football-guide-chatbot",
+    version="0.1.0",
+    author="Oseko Ashpe",
+    author_email="ayubu.oseko@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
