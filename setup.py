9aa51d4608cdc4bd3426695f29f75c34a8a82901
Oseko Ashpe <oskash6@gmail.com>
2026-08-07 21:28:00 +0300

    Remove unnecessary Python packaging files for Flask deployment

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="football-guide-chatbot",
-    version="0.1.0",
-    author="Oseko Ashpe",
-    author_email="ayubu.oseko@gmail.com",
-    py_modules=["app"],
-)
