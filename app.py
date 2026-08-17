5bd451ffd5254f5e3091aba938d0d7a20af679ae
Oseko Ashpe <oskash6@gmail.com>
2026-08-17 13:48:11 +0300

    change groq model

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ base_retriever = docsearch.as_retriever(
 
 # Use smaller model to save tokens 
 chatModel = ChatGroq(
-    model="llama-3.1-8b-instant",
+    model="openai/gpt-oss-20b",
     temperature=0.2
 )
 
